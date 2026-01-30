fcd4b63dc53edb3c49bc7dff8804d7dd1006d095
mafajardo23 <mafajardo@davidson.edu>
2026-01-30 11:53:14 -0500

    Initial Set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import starlight from '@astrojs/starlight';
 // Astro config docs https://astro.build/config
 export default defineConfig({
 	// required to publish on GH Pages
-	site: 'https://omundy.github.io',
+	site: 'https://mafajardo23.github.io',
 	base: '/dig345-devlog',
 	integrations: [
 		// Startlight-specific config
 		starlight({
-			title: 'Owen\'s DevLog',
+			title: 'Maria\'s DevLog',
 			sidebar: [
 				{ label: 'Schedule', autogenerate: { directory: 'schedule' }, },
 			],
